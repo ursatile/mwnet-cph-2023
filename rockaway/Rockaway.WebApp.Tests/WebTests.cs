744d775a92009b795d0054cb03a4b162000b147b
Dylan Beattie <dylan@dylanbeattie.net>
2023-08-28 14:13:45 +0200

    WebAppplicationFactory test examples for endpoints with external service dependencies

--- rockaway/Rockaway.WebApp.Tests/WebTests.cs
+++ rockaway/Rockaway.WebApp.Tests/WebTests.cs
@@ -1,24 +1,36 @@
-using Microsoft.AspNetCore.Mvc.Testing;
+using AngleSharp;
+using Shouldly;
 
 namespace Rockaway.WebApp.Tests;
 
 public class WebTests
 {
-	private HttpClient CreateClient()
-		=> new WebApplicationFactory<Program>().CreateClient();
+	protected IBrowsingContext browsingContext
+		=> BrowsingContext.New(Configuration.Default);
 
 	[Fact]
 	public async Task Status_Endpoint_Returns_OK()
 	{
-		var client = CreateClient();
+		var testDateTime = new DateTime(2023, 4, 5, 6, 7, 8);
+		var clock = new FakeClock(testDateTime);
+		var factory = new TestFactory(clock);
+		var client = factory.CreateClient();
 		var result = await client.GetAsync("/status");
 		result.EnsureSuccessStatusCode();
+		var html = await result.Content.ReadAsStringAsync();
+		var dom = await browsingContext.OpenAsync(req => req.Content(html));
+		var element = dom.QuerySelector("#system-time");
+		element.ShouldNotBeNull();
+		element.InnerHtml.ShouldBe(testDateTime.ToString("O"));
 	}
 
 	[Fact]
 	public async Task Privacy_Page_Returns_OK()
 	{
-		var client = CreateClient();
+		var testDateTime = new DateTime(2023, 4, 5, 6, 7, 8);
+		var clock = new FakeClock(testDateTime);
+		var factory = new TestFactory(clock);
+		var client = factory.CreateClient();
 		var result = await client.GetAsync("/privacy");
 		result.EnsureSuccessStatusCode();
 	}
@@ -26,7 +38,10 @@ public class WebTests
 	[Fact]
 	public async Task Hello_Api_Returns_OK()
 	{
-		var client = CreateClient();
+		var testDateTime = new DateTime(2023, 4, 5, 6, 7, 8);
+		var clock = new FakeClock(testDateTime);
+		var factory = new TestFactory(clock);
+		var client = factory.CreateClient();
 		var result = await client.GetAsync("/hello");
 		result.EnsureSuccessStatusCode();
 	}
