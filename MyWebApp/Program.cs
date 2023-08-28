1479cf4d0084e9ac4c68bdfaab171df5b9af1d21
Dylan Beattie <dylan@dylanbeattie.net>
2023-08-28 10:48:19 +0200

    Broken HTML from a minimal API

--- MyWebApp/Program.cs
+++ MyWebApp/Program.cs
@@ -1,11 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/hello/{name}", (string name) => $"Hello {name}!");
-app.MapGet("/hei/{name}", (string name) => $"Hei {name}!");
-app.MapGet("/multiply/{x}/{y}", (int x, int y) => x * y);
-app.MapGet("/multiply", (int x, int y) => x * y);
-app.MapGet("/object", () => new { greeting = "Hello", who = "Copenhagen" });
-app.MapGet("/form", () => Results.File("form.html", "text/html"));
+app.MapGet("/", () => Results.File("form.html", "text/html"));
+app.MapPost("/calculate", (HttpContext ctx) => {
+    Int32.TryParse(ctx.Request.Form["x"], out int x);
+    Int32.TryParse(ctx.Request.Form["y"], out int y);
+    var op = ctx.Request.Form["op"].Single();
+    var result = op switch {
+        "*" => x * y,
+        "-" => x - y,
+        "/" => x /y ,
+        _ => x + y
+    };
+    return Results.Content($"<!DOCTYPE html><html><body>The result is: {result}<hr /><small>Powered by World's Dumbest Calculator!/<small></body></html>", "text/html");
+});
+
+
+// app.MapGet("/hello/{name}", (string name) => $"Hello {name}!");
+// app.MapGet("/hei/{name}", (string name) => $"Hei {name}!");
+// app.MapGet("/multiply/{x}/{y}", (int x, int y) => x * y);
+// app.MapGet("/multiply", (int x, int y) => x * y);
+// app.MapGet("/object", () => new { greeting = "Hello", who = "Copenhagen" });
+// app.MapGet("/form", () => Results.File("form.html", "text/html"));
 app.Run();
